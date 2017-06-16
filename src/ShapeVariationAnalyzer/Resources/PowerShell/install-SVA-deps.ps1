fe9be3f805b0919828111e854c672d8da4b36a61
Priscille de Dumast <p.dedumast@gmail.com>
2017-06-16 13:28:18 -0400

    BUG: Fixed path

--- src/ShapeVariationAnalyzer/Resources/PowerShell/install-SVA-deps.ps1
+++ src/ShapeVariationAnalyzer/Resources/PowerShell/install-SVA-deps.ps1
@@ -3,7 +3,7 @@ trap { Write-Error $_; Exit 1 }
 $scriptName = "install-python.ps1"
 if (![System.IO.File]::Exists(".\windows\$scriptName")) {
   Write-Host "Download $scriptName"
-  $url = "https://raw.githubusercontent.com/pdedumast/ShapeVariationAnalyzer/master/src/ShapeVariationAnalyzer/Resources/PowerShell/windows$scriptName"
+  $url = "https://raw.githubusercontent.com/pdedumast/ShapeVariationAnalyzer/master/src/ShapeVariationAnalyzer/Resources/PowerShell/windows/$scriptName"
   $cwd = (Get-Item -Path ".\" -Verbose).FullName
   (new-object net.webclient).DownloadFile($url, "$cwd\$scriptName")
 }
